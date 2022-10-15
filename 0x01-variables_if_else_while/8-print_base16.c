0ce764d7c2e90260ba85b88d0c210085fbe10cab
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-15 02:36:24 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - print hexadecimals
+* Return: Always 0
+*/
+int main(void)
+{
+int num;
+char hex;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+for (hex = 'a'; hex <= 'f'; hex++);
+putchar('\n');
+return (0);
+}
