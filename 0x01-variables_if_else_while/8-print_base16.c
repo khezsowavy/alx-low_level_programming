3aa6246faebf20001ac3371a607622a7b19d9de7
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-15 03:52:16 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,11 @@ int num;
 char hx;
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
-
-for (hx = 'a'; hx <= 'f'; hx++);
+for (hx = 'a'; hx <= 'f');
+{  
 putchar(hx);
-
+hx++;
+}
 putchar('\n');
 return (0);
 }
