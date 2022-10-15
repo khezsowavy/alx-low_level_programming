01f8c5919724abeac87a136edeea8c020930aa68
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-15 02:01:02 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+/**
+* main - prints in lower and upper case
+* Return: Always 0
+*/
+int main(void)
+{
+char lu
+for (lu = 'a'; lu <= 'z'; lu++)
+putchar(lu);
+for (lu = 'A'; lu <= 'Z'; lu++)
+putchar(lu);
+putchar('\n');
+return (0);
+}
