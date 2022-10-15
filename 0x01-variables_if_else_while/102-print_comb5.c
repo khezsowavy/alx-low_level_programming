edc8a04d2b251cda598da63c770d206e9fde9493
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-15 05:15:30 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,3 +5,34 @@
 */
 int main(void)
 {
+int main(void)
+{
+int c = 0;
+int f_d;
+int l_d;
+int d_a
+int b_a
+while (c <= 99)
+{
+f_d = (c / 10 + '0');
+l_d = (c % 10 + '0');
+b_a = (c / 10 + '0');
+d_a = (c % 10 + '0');
+if (f_d < l_d)
+{
+putchar(f_d);
+putchar(l_d);
+if (c != 89)
+{
+putchar(',');
+putchar(' ');
+}
+if (b_a < f_d) and (d_a < l_d)
+{
+putchar(' ')
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
