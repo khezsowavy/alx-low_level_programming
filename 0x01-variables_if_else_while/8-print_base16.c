ce3e23e6c8548ab714d37f8d9e7cc825e10ad203
khezsowavy <dayved27@gmail.com>
2022-10-14 19:38:32 -0700

    minor

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,11 @@
 int main(void)
 {
 int num;
-char hx;
+char hex;
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
-for (hx = 'a'; hx <= 'f'; hx++);
+for (hex = 'a'; hex <= 'f'; hex++);
+putchar(hex);
 putchar('\n');
 return (0);
 }
