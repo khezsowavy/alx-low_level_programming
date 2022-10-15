40c5673dd7e3c176f048b02b35006cc181d0bca8
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-15 04:23:31 +0100

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - prints all digits combinations
+* Return: Always 0
+*/
+int main(void)
+{
+int n;
+for (n = 01; n <= 99; n++)
+{
+putchar(n)
+if (n != 98)
+{
+putchar(',');
+putchar(' ');
+}
+if (n = 10)
+}
+for (
+putchar('\n');
+return (0);
+}
