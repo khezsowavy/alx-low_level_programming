2b81a8ceb859619b7356c796fd4f563810f4aa50
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-15 04:40:21 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,17 +5,52 @@
 */
 int main(void)
 {
-int n;
-for (n = 01; n <= 99; n++);
-{
-putchar(n);
-if (n != 98)
-{
-putchar(',');
-putchar(' ');
-}
-if (n = 10)
-}
+
+	int c = 0;
+
+	int f_d;
+
+	int l_d;
+
+
+
+	while (c <= 99)
+
+	{
+
+		f_d = (c / 10 + '0');
+
+		l_d = (c % 10 + '0');
+
+
+
+		if (f_d < l_d)
+
+		{
+
+			putchar(f_d);
+
+			putchar(l_d);
+
+
+
+			if (c != 89)
+
+			{
+
+				putchar(',');
+
+				putchar(' ');
+
+			}
+
+		}
+
+		c++;
+
+	}
+
 putchar('\n');
+
 return (0);
 }
