996ebc30033fd7f8900b985dfd90e8137e16633a
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-21 05:16:48 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,10 +5,8 @@
  */
 void print_numbers(void)
 {
-	char a;
-	for (a = '0'; a <= '9'; a++)
-	{
-		_putchar(a);
-	}
-	_putchar('\n');
+int a = 0;
+while (a < 10)
+_putchar(a++ + '0');
+_putchar('\n');
 }
