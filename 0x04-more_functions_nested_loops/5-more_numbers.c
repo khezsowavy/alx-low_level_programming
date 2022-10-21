83ac10db922989e7726c1f695c2147f83818db6b
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-21 17:50:16 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * print_most_numbers - functions that prints the numbers from 0 to 9
+ * more_numbers - prints 10 times numbers from 0 to 14
  * Return: void
  */
 
-void print_most_numbers(void)
+void more_numbers(void)
 {
-	char c;
-	
-	for (c = '0'; c <= '9'; c++)
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
-	}
-	_putchar('\n');
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+			_putchar('\n');
+		}
 }
