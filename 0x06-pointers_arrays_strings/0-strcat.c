17b6de1c2fd6ed6b964d91bb59a5b4ca889ba88e
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-10-27 03:16:18 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,14 +9,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i;
+	int j;
 
 	i = 0;
+while (dest[i] != '\0')
+{
+	i++;
+}
 	j = 0;
-
-	while (dest[i] != '\0')
-		i++;
-
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -24,6 +25,5 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
-
 	return (dest);
 }
