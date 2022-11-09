b5e8571f50f5070670cf09dfbefdc7166cfe6206
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-11-09 00:41:10 -0100

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - a function that checks for alphabetic character
+ *
+ * @c: single letter input
+ *
+ * Return: if c is upper or lower letter 1 else 0
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
