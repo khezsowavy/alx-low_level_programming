167b370feec9b05bf7a2a5e472d526498b501aed
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-11-09 00:12:27 -0100

    Create 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit through 0 and 9
+ * @c: a digit
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
