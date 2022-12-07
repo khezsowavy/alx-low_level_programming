abd1a80305fac25bb56afc0c9be6133f6b148ea3
Khez <106893248+khezsowavy@users.noreply.github.com>
2022-12-07 19:26:48 -0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,73 +1,37 @@
 #include "main.h"
 
-
-
 /**
+ * read_textfile - reads a text file and print the letters
+ * @filename: filename
+ * @letters: numbers of letters printed
  *
- *  * read_textfile - reads a text file and print the letters
- *
- *   * @filename: filename
- *
- *    * @letters: numbers of letters printed
- *
- *     *
- *
- *      * Return: numbers of letters printed or 0 if failure
- *
- *       */
-
-
+ * Return: numbers of letters printed or 0 if failure
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
-
 {
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-		int fd;
+	if (!filename)
+		return (0);
 
-			ssize_t nrd, nwr;
+	fd = open(filename, O_RDONLY);
 
-				char *buf;
+	if (fd == -1)
+		return (0);
 
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
-					if (!filename)
+	close(fd);
 
-								return (0);
+	free(buf);
 
-
-
-						fd = open(filename, O_RDONLY);
-
-
-
-							if (fd == -1)
-
-										return (0);
-
-
-
-								buf = malloc(sizeof(char) * (letters));
-
-									if (!buf)
-
-												return (0);
-
-
-
-										nrd = read(fd, buf, letters);
-
-											nwr = write(STDOUT_FILENO, buf, nrd);
-
-
-
-												close(fd);
-
-
-
-													free(buf);
-
-
-
-														return (nwr);
-
+	return (nwr);
 }
