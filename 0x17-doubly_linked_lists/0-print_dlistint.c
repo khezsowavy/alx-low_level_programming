3b2ee2b5b6f2f98d0b6c9f6d64ee4725c0118186
Khez <106893248+khezsowavy@users.noreply.github.com>
2024-01-26 10:58:04 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - thisprintsall the elements of a
+ * print_dlistint - this prints all the elements of a
  * dlistint_t list
  *
  * @h: head of the list
